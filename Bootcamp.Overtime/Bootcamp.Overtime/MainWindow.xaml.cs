112c4622c4d4b9c48416d1c75a7fc9475ecd322e
Wahidatin HR <wahrahmah496@gmail.com>
2019-01-31 15:12:25 +0700

    WPF Position

--- Bootcamp.Overtime/Bootcamp.Overtime/MainWindow.xaml.cs
+++ Bootcamp.Overtime/Bootcamp.Overtime/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Overtime.BusinessLogic.Services.Master;
+using Overtime.DataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,87 @@ namespace Bootcamp.Overtime
     /// </summary>
     public partial class MainWindow : Window
     {
+        MyContex _context = new MyContex();
+        iPositionService _positionService = new PositionService();
         public MainWindow()
         {
+            // 
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid1.ItemsSource = _context.Position.Where(x => x.isDelete == false).ToList();
+        }
+
+        private void buttonInsertPosition_Click(object sender, RoutedEventArgs e)
+        {
+            new PopUpInsertPosition().Show();
+            this.Hide();
+        }
+
+        private void buttonUpdatePosition_Click(object sender, RoutedEventArgs e)
+        {
+            PopUpUpdatePosition popUpUpdatepos = new PopUpUpdatePosition();
+            object items = dataGrid1.SelectedItem;
+            if (items == null)
+            {
+                MessageBox.Show("No Data selected for updating");
+            }
+            else
+            {
+                popUpUpdatepos.idtextBox.Text = (dataGrid1.SelectedCells[0].Column.GetCellContent(items) as TextBlock).Text;
+                popUpUpdatepos.textBoxPosition.Text = (dataGrid1.SelectedCells[1].Column.GetCellContent(items) as TextBlock).Text;
+                popUpUpdatepos.Show();
+                this.Hide();
+            }
+           
+            //new PopUpUpdatePosition().Show();
+        }
+
+        private void buttonDeletePosition_Click(object sender, RoutedEventArgs e)
+        {
+            object items = dataGrid1.SelectedItem;
+
+            if (items == null)
+            {
+                MessageBox.Show("No Data Selected to Delete");
+            }
+            else
+            {
+
+                MessageBoxResult result = MessageBox.Show("Are You Sure to Delete this Position ?", "WARNING", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+
+                    int id = Convert.ToInt16((dataGrid1.SelectedCells[0].Column.GetCellContent(items) as TextBlock).Text);
+                    _positionService.delete(id);
+                    dataGrid1.ItemsSource = _positionService.Get();
+
+                }
+            }
+         
+            //new PopUpDeletePosition().Show();
+        }
+
+        private void buttonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+            dataGrid1.ItemsSource = _context.Position.Where(x => x.isDelete == false).ToList();
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid1.ItemsSource = _positionService.Search(textBoxSearch.Text, SearchComboBox.Text);
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        public void LoadGrid()
+        {
+            dataGrid1.ItemsSource = _positionService.Get();
         }
     }
 }
